a822495f8ae790e0eddeecd63137cdbfc0a2b201
2mmtmc@gmail.com <mathieudemilloterrazzani@iMac-de-Mathieu-2.local>
2019-07-28 23:28:35 +0200

    Ajout de commentaires à la fonction d'hydratation de chaque entité

--- src/Model/Entities/User.php
+++ src/Model/Entities/User.php
@@ -17,8 +17,11 @@ class User {
     
     public function hydrate(array $data) {
         foreach ($data as $key => $value) {
+            /* On récupère le nom du setter correspondant à l'attribut. */
             $method = 'set' . ucfirst($key);
+            /* Si le setter correspondant existe */
             if (method_exists($this, $method)){
+                /* On appelle le setter */
                 $this->$method($value);
             }
         }
